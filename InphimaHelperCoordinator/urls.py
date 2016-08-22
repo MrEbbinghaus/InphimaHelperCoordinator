d2b8faa44617ff2ab7e6e2b3083bb92b7cd679dd
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-08-22 15:04:59 +0200

    Add buttons to subpages

--- InphimaHelperCoordinator/urls.py
+++ InphimaHelperCoordinator/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib.auth.views import login, logout
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     url(r'^login/$', login, name="login"),
     url(r'^logout/$', logout, name="logout"),
-    url(r'^pss/', include("PartyShiftSchedule.urls")),
+    url(r'^pss/', include("PartyShiftSchedule.urls", namespace='PartyShiftSchedule')),
     url(r'^admin/', admin.site.urls, name="admin"),
-    url(r'^$', login, name="login")
+    url(r'^$', RedirectView.as_view(url='/pss')),
+
 ]
