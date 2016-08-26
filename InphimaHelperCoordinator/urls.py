819b65c5d36b2c033c4c676dd68615663ed8d6dc
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-08-26 13:55:12 +0200

    Toggle Buttons now preset
    fixes #10

--- InphimaHelperCoordinator/urls.py
+++ InphimaHelperCoordinator/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-#from django.contrib.auth.views import login, logout
 from django.contrib import admin
 from django.views.generic import RedirectView
 
