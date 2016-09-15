2d42880715ade1eb810c7a36cae98412c8b7b46e
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-09-15 23:14:07 +0200

    Add static files to url

--- InphimaHelperCoordinator/urls.py
+++ InphimaHelperCoordinator/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import RedirectView
 
@@ -22,4 +25,4 @@ urlpatterns = [
     url(r'^pss/', include("PartyShiftSchedule.urls", namespace='PartyShiftSchedule')),
     url(r'^admin/', admin.site.urls, name="admin"),
     url(r'^$', RedirectView.as_view(url='/pss')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
